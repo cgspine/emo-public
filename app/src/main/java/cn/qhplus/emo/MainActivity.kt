65b69312415a757b9641e60ed48fe00efae14c65
cgspine <cgspine@gmail.com>
2022-06-13 16:03:27 +0800

    feat: icons.

--- app/src/main/java/cn/qhplus/emo/MainActivity.kt
+++ app/src/main/java/cn/qhplus/emo/MainActivity.kt
@@ -15,36 +15,28 @@
  */
 package cn.qhplus.emo
 
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import cn.qhplus.emo.ui.EmoApp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
-class MainActivity: ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-            window.navigationBarDividerColor = Color.TRANSPARENT
-            window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
-        }
         setContent {
             EmoApp(calculateWindowSizeClass(this))
         }
         lifecycleScope.launch {
-            delay(100)
+            delay(50)
             window.setBackgroundDrawableResource(android.R.color.transparent)
         }
     }
