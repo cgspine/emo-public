f114896d35161f0f73ee810a3d4dfd532f8f8715
cgspine <cgspine@gmail.com>
2022-10-08 22:26:52 +0800

    feat: update compose dependencies.

--- ui-core/build.gradle.kts
+++ ui-core/build.gradle.kts
@@ -34,12 +34,6 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
 
-    // TODO : Remove these dependency once we upgrade to Android Studio Dolphin b/228889042
-    // These dependencies are currently necessary to render Compose previews
-    debugImplementation(libs.androidx.customview.poolingcontainer)
-    debugImplementation(libs.androidx.lifecycle.viewModelCompose)
-    debugImplementation(libs.androidx.savedstate.ktx)
-
     api(project(":core"))
 
     api(libs.androidx.compose.foundation)
@@ -50,4 +44,6 @@ dependencies {
     api(libs.androidx.compose.ui.tooling.preview)
     api(libs.androidx.compose.ui.util)
     api(libs.androidx.compose.runtime)
+    api(libs.androidx.lifecycle.runtimeCompose)
+    api(libs.androidx.lifecycle.viewModelCompose)
 }
