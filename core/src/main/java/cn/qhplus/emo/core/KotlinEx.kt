e72909b75b69d4bef4ee0bb5852d3bf653643ff9
cgspine <cgspine@gmail.com>
2023-02-08 20:21:12 +0800

    feat: add method runIfNotEmpty.

--- core/src/main/java/cn/qhplus/emo/core/KotlinEx.kt
+++ core/src/main/java/cn/qhplus/emo/core/KotlinEx.kt
@@ -20,6 +20,12 @@ import java.io.Closeable
 
 inline fun <T> T.runIf(condition: Boolean, block: T.() -> T): T = if (condition) block() else this
 
+inline fun <T> List<T>.runIfNotEmpty(block: List<T>.() -> Unit) {
+    if(isNotEmpty()){
+        block()
+    }
+}
+
 fun Closeable.closeQuietly() {
     try {
         close()
--- core/src/main/java/cn/qhplus/emo/core/Retry.kt
+++ core/src/main/java/cn/qhplus/emo/core/Retry.kt
@@ -16,10 +16,9 @@
 
 package cn.qhplus.emo.core
 
-import kotlinx.coroutines.delay
 import java.util.concurrent.CancellationException
 
-fun <T> retry(times: Int, block: () -> T): T {
+inline fun <T> retry(times: Int, block: () -> T): T {
     var throwable: Throwable? = null
     for (i in 0 until times) {
         try {
@@ -29,24 +28,6 @@ fun <T> retry(times: Int, block: () -> T): T {
             if (e is CancellationException) {
                 break
             }
-        }
-    }
-    throw throwable ?: RuntimeException("failed after retry $times times")
-}
-
-suspend fun <T> retry2(times: Int, failedDelay: Long = 0, block: suspend () -> T): T {
-    var throwable: Throwable? = null
-    for (i in 0 until times) {
-        try {
-            return block()
-        } catch (e: Throwable) {
-            throwable = e
-            if (e is CancellationException) {
-                break
-            }
-        }
-        if (failedDelay > 0) {
-            delay(failedDelay)
         }
     }
     throw throwable ?: RuntimeException("failed after retry $times times")
